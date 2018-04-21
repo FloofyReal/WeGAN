f14a7b349c8aabb42cd9f66afff68c0770905cfe
FloofyReal <adam.rafajdus@gmail.com>
2018-04-21 18:19:57 +0200

    Fix paths to make metadata
    Signed-off-by: FloofyReal <adam.rafajdus@gmail.com>

--- extra/save_metadata.py
+++ extra/save_metadata.py
@@ -1,17 +1,17 @@
 import numpy as np
 import pickle
 
-name = '../../parsed_data/32x32/train_Temperature.pkl'
-name2 = '../../parsed_data/32x32/train_Cloud_cover.pkl'
-name3 = '../../parsed_data/32x32/train_Specific_humidity.pkl'
+name = '/home/rafajdus/data_parsed/32x32/train_Temperature_32x32.pkl'
+name2 = '/home/rafajdus/data_parsed/32x32/train_Cloud_cover_32x32.pkl'
+name3 = '/home/rafajdus/data_parsed/32x32/train_Specific_humidity_32x32.pkl'
 
 names_32 = [name, name2, name3]
 
-name = '../../parsed_data/64x64/train_Temperature.pkl'
-name2 = '../../parsed_data/64x64/train_Cloud_cover.pkl'
-name3 = '../../parsed_data/64x64/train_Specific_humidity.pkl'
-name4 = '../../parsed_data/64x64/train_Logarithm_of_surface_pressure.pkl'
-name5 = '../../parsed_data/64x64/train_Geopotential.pkl'
+name = '/home/rafajdus/data_parsed/64x64/train_Temperature_64x64.pkl'
+name2 = '/home/rafajdus/data_parsed/64x64/train_Cloud_cover_64x64.pkl'
+name3 = '/home/rafajdus/data_parsed/64x64/train_Specific_humidity_64x64.pkl'
+name4 = '/home/rafajdus/data_parsed/64x64/train_Logarithm_of_surface_pressure_64x64.pkl'
+name5 = '/home/rafajdus/data_parsed/64x64/train_Geopotential_64x64.pkl'
 
 names_64 = [name, name2, name3, name4, name5]
 
@@ -64,14 +64,14 @@ norm2 = ((hum-mmm)/(std))
 print('min:',np.min(norm2),'max:', np.max(norm2))
 hhh = {'Specific_humidity_mean':mmm, 'Specific_humidity_std':std}
 
-meta32 = {ttt,ccc,hhh}
-with open('../../parsed_data/32x32/meta.pkl','wb') as f:
+meta32 = {**ttt, **ccc, **hhh}
+with open('/home/rafajdus/data_parsed/32x32/meta32x32.pkl','wb') as f:
     pickle.dump(meta32, f)
 
 data_all = []
 for x in data_64:
     data_values = [i[0] for i in x]
-    data_values = [i.reshape([1,1,32,32,1]) for i in data_values]
+    data_values = [i.reshape([1,1,64,64,1]) for i in data_values]
     data_v = np.concatenate(data_values, axis=0)
     data_all.append(data_v)
 data_all = np.concatenate(data_all, axis=4)
@@ -123,6 +123,6 @@ norm2 = ((sp-mmm)/(std))
 print('min:',np.min(norm2),'max:', np.max(norm2))
 sss = {'Logarithm_of_surface_pressure_mean':mmm, 'Logarithm_of_surface_pressure_std':std}
 
-meta64 = {ttt,ccc,hhh,ggg,sss}
-with open('../../parsed_data/64x64/meta.pkl','wb') as f:
+meta64 = {**ttt, **ccc, **hhh, **ggg, **sss}
+with open('/home/rafajdus/data_parsed/64x64/meta64x64.pkl','wb') as f:
     pickle.dump(meta64, f)
