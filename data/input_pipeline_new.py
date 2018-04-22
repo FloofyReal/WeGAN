40440749a8a890d6aa62686c071c201ca2c639a4
FloofyReal <adam.rafajdus@gmail.com>
2018-04-22 02:46:20 +0200

    Fix missing ]
    Signed-off-by: FloofyReal <adam.rafajdus@gmail.com>

--- data/input_pipeline_new.py
+++ data/input_pipeline_new.py
@@ -130,7 +130,7 @@ class InputPipeline(object):
         seq_tensor = [tf.convert_to_tensor(normals[x:x+self.video_frames], np.float32) for x in range(normals.shape[0]-self.video_frames)]
         seq_tensor = [tf.reshape(tt, [1, self.video_frames, self.reshape_size, self.reshape_size, self.channels]) for tt in seq_tensor]
 
-        time_tensor = [x for x in range(times.shape[0]-self.videos)
+        time_tensor = [x for x in range(times.shape[0]-self.videos)]
         # time_tensor = [tf.convert_to_tensor(times[x:self.video_frames+x], np.float32) for x in range(normals.shape[0]-self.video_frames)]
         # time_tensor = [tf.reshape(tt, [1, self.video_frames, 2]) for tt in time_tensor]
 
