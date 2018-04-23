931aecc6d1f632eded11395c6a6e99cbdee62493
FloofyReal <adam.rafajdus@gmail.com>
2018-04-23 05:33:26 +0200

    Update defaults
    Signed-off-by: FloofyReal <adam.rafajdus@gmail.com>

--- train.sh
+++ train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 name=$1
+channels=$1
+wvars=$1
 
-python main_train.py --mode=predict_1to1 --experiment_name=$name
+python main_train.py --mode=predict_1to1 --experiment_name=$name --channels=3 --wvars='11100'
