a781e9667a6aae748b4144c682ab1d828d2b9a65
FloofyReal <adam.rafajdus@gmail.com>
2018-04-24 07:30:06 +0200

    Add option to reduce 64x64 dataset to 32x32
    Signed-off-by: FloofyReal <adam.rafajdus@gmail.com>

--- extra/split.py
+++ extra/split.py
@@ -24,6 +24,12 @@ def split_time_dataset(data, test=0.1):
     
     return train_features, test_features
 
+def from64to32(data):
+    print(data[0][0].shape)
+    new = [[i[0][15:15+32,16:16+32],i[1]] for i in data]
+    print(new[0][0].shape)
+    return new
+
 def save_dataset(train_data, valid_data, test_data, source_path, param, dirr):
     # saving dataset as parts
     
@@ -48,8 +54,8 @@ def save_dataset(train_data, valid_data, test_data, source_path, param, dirr):
 
 ##############################################################################
 # MAIN CHANGABLE VARIABLES
-# dirr = '32x32'
-dirr = '64x64'
+dirr = '32x32'
+# dirr = '64x64'
 
 params = ['Temperature', 'Specific_humidity', 'Cloud_cover']
 params_64 = ['Geopotential', 'Logarithm_of_surface_pressure']
@@ -57,56 +63,110 @@ params_64 = ['Geopotential', 'Logarithm_of_surface_pressure']
 # path to whole .pkl of data
 source_path = '../../data_parsed/' + dirr + '/'
 
+# reduced = True
+reduced = False
+
 # Supervised learning
 # 80:10:10
 # data_train, data_valid, data_test = split_dataset(data, valid=0.1, test=0.1)
 # save_dataset(data_train, data_valid, data_test, source_path, params, dirr)
 
-
-# Unsupervised learning
-for param in params:
-    fin_file = param + '.pkl'
-    
-    print(fin_file)
-    # loading dataset
-    with open(source_path + fin_file, 'rb') as f:
-        data = pickle.load(f, encoding='bytes')
-
-    # no valid - 90:10
-    
-    # NO TIME
-    # data_train, data_valid, data_test = split_dataset(data, valid=0, test=0.1)
-    # save_dataset(data_train, data_valid, data_test, source_path, params, dirr)
-    
-    # WITH TIME
-    data_train_time, data_test_time = split_time_dataset(data, test=0.1)
-    print(len(data_train_time))
-    print(data_train_time[0][1].ctime())
-    
-    print(len(data_test_time))
-    print(data_test_time[0][1].ctime())
-    save_dataset(data_train_time, [], data_test_time, source_path, param, dirr)
-
-if dirr == '64x64':
-    for param in params_64:
+if not reduced:
+    # Unsupervised learning
+    for param in params:
         fin_file = param + '.pkl'
-
+        
         print(fin_file)
         # loading dataset
         with open(source_path + fin_file, 'rb') as f:
             data = pickle.load(f, encoding='bytes')
 
         # no valid - 90:10
-
+        
         # NO TIME
         # data_train, data_valid, data_test = split_dataset(data, valid=0, test=0.1)
         # save_dataset(data_train, data_valid, data_test, source_path, params, dirr)
-
+        
         # WITH TIME
         data_train_time, data_test_time = split_time_dataset(data, test=0.1)
         print(len(data_train_time))
         print(data_train_time[0][1].ctime())
-
+        
         print(len(data_test_time))
         print(data_test_time[0][1].ctime())
         save_dataset(data_train_time, [], data_test_time, source_path, param, dirr)
+
+    if dirr == '64x64':
+        for param in params_64:
+            fin_file = param + '.pkl'
+
+            print(fin_file)
+            # loading dataset
+            with open(source_path + fin_file, 'rb') as f:
+                data = pickle.load(f, encoding='bytes')
+
+            # no valid - 90:10
+
+            # NO TIME
+            # data_train, data_valid, data_test = split_dataset(data, valid=0, test=0.1)
+            # save_dataset(data_train, data_valid, data_test, source_path, params, dirr)
+
+            # WITH TIME
+            data_train_time, data_test_time = split_time_dataset(data, test=0.1)
+            print(len(data_train_time))
+            print(data_train_time[0][1].ctime())
+
+            print(len(data_test_time))
+            print(data_test_time[0][1].ctime())
+            save_dataset(data_train_time, [], data_test_time, source_path, param, dirr)
+else:
+    # Unsupervised learning
+    for param in params:
+        fin_file = param + '.pkl'
+        
+        print(fin_file)
+        # loading dataset
+        with open(source_path + fin_file, 'rb') as f:
+            data = pickle.load(f, encoding='bytes')
+
+        data = from64to32(data)
+
+        # no valid - 90:10
+        
+        # NO TIME
+        # data_train, data_valid, data_test = split_dataset(data, valid=0, test=0.1)
+        # save_dataset(data_train, data_valid, data_test, source_path, params, dirr)
+        
+        # WITH TIME
+        data_train_time, data_test_time = split_time_dataset(data, test=0.1)
+        print(len(data_train_time))
+        print(data_train_time[0][1].ctime())
+        
+        print(len(data_test_time))
+        print(data_test_time[0][1].ctime())
+        save_dataset(data_train_time, [], data_test_time, source_path, param, dirr)
+
+    if dirr == '64x64':
+        for param in params_64:
+            fin_file = param + '.pkl'
+
+            print(fin_file)
+            # loading dataset
+            with open(source_path + fin_file, 'rb') as f:
+                data = pickle.load(f, encoding='bytes')
+
+            data = from64to32(data)
+            # no valid - 90:10
+
+            # NO TIME
+            # data_train, data_valid, data_test = split_dataset(data, valid=0, test=0.1)
+            # save_dataset(data_train, data_valid, data_test, source_path, params, dirr)
+
+            # WITH TIME
+            data_train_time, data_test_time = split_time_dataset(data, test=0.1)
+            print(len(data_train_time))
+            print(data_train_time[0][1].ctime())
+
+            print(len(data_test_time))
+            print(data_test_time[0][1].ctime())
+            save_dataset(data_train_time, [], data_test_time, source_path, param, dirr)
